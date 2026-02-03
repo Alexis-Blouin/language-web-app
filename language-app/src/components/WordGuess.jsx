495ee2d7a864da30e0057aff071ceacc9b25548f
Alexis-Blouin <116613522+Alexis-Blouin@users.noreply.github.com>
2026-02-03 17:46:46 -0500

    Adding a button to reload the word adn reload when getting the correct answer

--- language-app/src/components/WordGuess.jsx
+++ language-app/src/components/WordGuess.jsx
@@ -3,6 +3,9 @@ import "react-simple-toasts/dist/style.css"; // Will give a warning, but works a
 import "react-simple-toasts/dist/theme/dark.css";
 import "react-simple-toasts/dist/theme/success.css";
 import "react-simple-toasts/dist/theme/failure.css";
+import { useState } from "react";
+
+import reload from "../assets/images/reload.png";
 
 // specify the theme in toastConfig
 toastConfig({
@@ -10,39 +13,67 @@ toastConfig({
 });
 
 function WordGuess({ words }) {
-  const randomWord = words[Math.floor(Math.random() * words.length)];
-  const hideHanzi = Math.random() < 0.5;
-  const [question, answer] = hideHanzi
-    ? [randomWord.translation, randomWord.hanzi]
-    : [randomWord.hanzi, randomWord.translation];
+  const pickRandomWord = () => {
+    const randomWord = words[Math.floor(Math.random() * words.length)];
+    const hideHanzi = Math.random() < 0.5;
+
+    return {
+      question: hideHanzi ? randomWord.translation : randomWord.hanzi,
+      answer: hideHanzi ? randomWord.hanzi : randomWord.translation,
+      hideHanzi,
+    };
+  };
+  const [word, setWord] = useState(pickRandomWord);
+
+  const changeWord = () => {
+    setWord(pickRandomWord());
+  };
 
   return (
     <div id="wordGuess">
-      <Question question={question} hideHanzi={hideHanzi} />
-      <Guess answer={answer} hideHanzi={!hideHanzi} />
+      <Question
+        question={word.question}
+        hideHanzi={word.hideHanzi}
+        changeWord={changeWord}
+      />
+      <Guess
+        answer={word.answer}
+        hideHanzi={!word.hideHanzi}
+        changeWord={changeWord}
+      />
     </div>
   );
 }
 
 export default WordGuess;
 
-function Question({ question, hideHanzi }) {
+function Question({ question, hideHanzi, changeWord }) {
   const title = hideHanzi ? "Translation" : "Hanzi";
+  // <a href="https://www.flaticon.com/free-icons/reload" title="reload icons">Reload icons created by shin_icons - Flaticon</a>
   return (
     <div id="questionBox" className="guessContent">
-      <p>{title}:</p>
-      <p id="questionText">{question}</p>
+      <p className="fullWidth">{title}:</p>
+      <div>
+        <p id="questionText" className="fullWidth">
+          {question}
+          <button id="changeGuessButton" onClick={changeWord}>
+            <img src={reload} alt="Reload" />
+          </button>
+        </p>
+      </div>
     </div>
   );
 }
 
-function Guess({ answer, hideHanzi }) {
+function Guess({ answer, hideHanzi, changeWord }) {
   const title = hideHanzi ? "Translation" : "Hanzi";
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const guess = event.target.guess.value;
     if (guess === answer) {
+      event.target.guess.value = "";
+      changeWord();
       toast("Correct!", { theme: "success" });
     } else {
       toast("Incorrect...", { theme: "failure" });
@@ -51,10 +82,16 @@ function Guess({ answer, hideHanzi }) {
 
   return (
     <div id="guessBox" className="guessContent">
-      <p>{title}:</p>
+      <p className="fullWidth">{title}:</p>
       <form id="guessForm" onSubmit={handleSubmit}>
         <label htmlFor="guess">
-          <input type="text" name="guess" id="guess" placeholder="answer" />
+          <input
+            className="fullWidth"
+            type="text"
+            name="guess"
+            id="guess"
+            placeholder="answer"
+          />
         </label>
         <button id="guessButton">Confirm</button>
       </form>
